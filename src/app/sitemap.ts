63850997e117cb8d59cfd6f2bcb4b0eb87a07bef
Tomasz Talko <tomasztalko@laptop-tomasz.home>
2025-08-02 18:37:38 +0200

    Fix robots.txt and sitemap.ts for static export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://gentle-tree-02afc0e0f.2.azurestaticapps.net'
   
